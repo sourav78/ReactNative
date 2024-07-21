89319fb14922e82b76ec6dedcb61cf49a58ffd9d
sourav78 <souravranjan488@gmail.com>
2024-07-21 21:48:19 +0530

    aura - define file structure

--- Aura/app/index.tsx
+++ Aura/app/index.tsx
@@ -1,14 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { Link } from 'expo-router'
 
 export default function _layout() {
   return (
     <View style={styles.container}>
-      <Text className='text-4xl'>Hello react native</Text>
+      <Text className='text-4xl font-pblack'>Aura</Text>
       <StatusBar style='auto'/>
-      <Link href={"/profile"} style={{color: "blue"}}>Profile</Link>
     </View>
   )
 }
